b68db8209a5f855f8edf5660c92dbc44f5fc6446
takefumi-yoshii <takefumi.yoshii@dena.com>
2018-03-27 22:20:54 +0900

    fix preload state bug

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
 import mitt from 'mitt'
 
-const putTakeMiddleware = store => next => action => {
-  const m = mitt()
-  const store = next(action)
+const putTakeMiddleware = () => {
+  return createStore => (...args) => {
+    const m = mitt()
+    const store = createStore(...args)
 
-  const dispatch = action => {
-    m.emit(action.type, action)
-    return store.dispatch(action)
-  }
-
-  const subscribe = (type, handler) => {
-    m.on(type, handler)
-    return () => m.off(type, handler)
-  }
-
-  const take = resolver => {
-    if (typeof resolver === 'string') {
-      return new Promise(resolve => {
-        const unsubscribe = subscribe(resolver, action => {
-          unsubscribe()
-          resolve(action)
-        })
-      })
+    const dispatch = action => {
+      m.emit(action.type, action)
+      return store.dispatch(action)
     }
-    if (typeof resolver === 'function') {
-      return new Promise(resolve => {
-        const unsubscribe = subscribe('*', (type, action) => {
-          if (resolver(action)) {
+
+    const subscribe = (type, handler) => {
+      m.on(type, handler)
+      return () => m.off(type, handler)
+    }
+
+    const take = resolver => {
+      if (typeof resolver === 'string') {
+        return new Promise(resolve => {
+          const unsubscribe = subscribe(resolver, action => {
             unsubscribe()
             resolve(action)
-          }
+          })
         })
+      }
+      if (typeof resolver === 'function') {
+        return new Promise(resolve => {
+          const unsubscribe = subscribe('*', (type, action) => {
+            if (resolver(action)) {
+              unsubscribe()
+              resolve(action)
+            }
+          })
+        })
+      }
+    }
+
+    const put = action => {
+      return new Promise(resolve => {
+        m.emit(action.type, action)
+        store.dispatch(action)
+        resolve(action)
       })
     }
-  }
 
-  const put = action => {
-    return new Promise(resolve => {
-      m.emit(action.type, action)
-      store.dispatch(action)
-      resolve(action)
-    })
-  }
-
-  return {
-    ...store,
-    dispatch,
-    take,
-    put
+    return {
+      ...store,
+      dispatch,
+      take,
+      put
+    }
   }
 }
 
